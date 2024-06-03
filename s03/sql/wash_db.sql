6ba7a2dc9ac83738a5cb1a7d1bdd27401f66752f
Dmitry <63024074+vfx-beavers@users.noreply.github.com>
2024-06-03 20:15:13 +0300

    Delete s03 directory

--- s03/sql/wash_db.sql
+++
@@ -1,7 +0,0 @@
-truncate table staging.user_order_log cascade;
---truncate table mart.d_calendar;
-truncate table mart.d_city cascade;
-truncate table mart.d_customer cascade;
-truncate table mart.d_item cascade;
-truncate table mart.f_sales cascade;
-truncate table mart.f_customer_retention cascade;
